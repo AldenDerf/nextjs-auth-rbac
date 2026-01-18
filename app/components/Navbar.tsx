b0caa8f6f345aa372996cc48f443c4a48c44a1b2
Alden Derf <aldenderfc.fabro99@gmail.com>
2026-01-18 17:06:06 +0800

    re learned the userServerSession and useSession
    re learned the userServerSession and useSession

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,6 +6,7 @@ import { LogoutButton } from "./LogoutButton";
 export default function NavBar() {
     // This hool listens to the SessionProvider automatically
     const { data: session} = useSession();
+    console.log(session);
 
     return (
         <nav style={{ padding: 10, borderBottom: "1px solid #ccc", minHeight: "50px"}}>
