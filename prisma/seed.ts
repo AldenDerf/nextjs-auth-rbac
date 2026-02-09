812421367b023c7c34788b3fc8be41d728b62d87
Alden Derf <aldenderfc.fabro99@gmail.com>
2026-02-09 21:02:03 +0800

    fix the prisma error

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,11 +1,22 @@
 import { PrismaClient } from "./generated/client";
+import "dotenv/config";
 import bcrypt from "bcrypt";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+console.log("Database URL present:", !!process.env.DATABASE_URL);
 
 async function main() {
   const password = await bcrypt.hash("password123", 10);
 
   await prisma.user.upsert({
-    where: { email: "admin@.com" },
+    where: { email: "admin@test.com" },
     update: {},
     create: {
       email: "admin@test.com",
@@ -15,4 +26,12 @@ async function main() {
   });
 }
 
-main();
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
